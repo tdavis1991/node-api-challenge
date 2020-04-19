d1ca9a9b99f8880a197f22d60cf3c789941bdb98
tevin davis <aod.td1105@gmail.com>
2020-04-19 19:47:50 -0400

    Setting up boiler plate for index.js file and added a router file

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+const port = 4000;
+
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.listen(port, () => {
+    console.log(`Listening to http://localhost:${port}`)
+})
